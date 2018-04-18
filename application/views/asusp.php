1d43bdd86c79306f2f08410b74035ee5aadbe982
Ahmed_Tolba <a.h.tolba95@gmail.com>
2018-04-18 05:22:50 -0700

    setting up

---
+++ application/views/asusp.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ASUsp</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/main.css">
+</head>
+<body>
+
+</body>
+</html>
