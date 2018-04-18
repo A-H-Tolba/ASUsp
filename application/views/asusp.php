495c4e6da6be450f6558fab86c5a3c9046f52069
Ahmed_Tolba <a.h.tolba95@gmail.com>
2018-04-18 07:23:33 -0700

    fixing signup form

--- application/views/asusp.php
+++ application/views/asusp.php
@@ -36,7 +36,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		</nav>
 
 		<div class="container-fluid">
-		<form name="myForm" autocomplete="off" enctype="multipart/form-data" method="post" action="<?php echo base_url(); ?>Welcome/login">
+		<form name="myForm" autocomplete="off" enctype="multipart/form-data" method="post" action="<?php echo base_url(); ?>Welcome/signup">
 			<input type="text" name="username" class="form-control" placeholder="Username">
 			<input type="email" name="email" class="form-control" placeholder="E-Mail">
 			<input type="password" name="password" class="form-control" placeholder="Password">
