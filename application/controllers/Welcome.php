0c04fb6d90f6d55071f8777f833f0083e79311cc
Ahmed_Tolba <a.h.tolba95@gmail.com>
2018-04-18 06:55:22 -0700

    solving conflict

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->model('Users_model');
+	}
 	public function index()
 	{
 		$this->load->view('asusp');
