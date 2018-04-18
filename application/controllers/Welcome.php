cd37997108dcd963445d4a2b1ef0be8ec95b5ce1
Ahmed Adel <AhmedAdel94@users.noreply.github.com>
2018-04-18 15:01:37 +0200

    Sign up
    Sign up function

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -7,4 +7,21 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('asusp');
 	}
+	public function signup()
+	{
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('email', 'email', 'required|is_unique[users.email]|valid_email');
+		if ($this->form_validation->run() == FALSE)
+		{
+			$valid['valid']=FALSE;
+		}
+        else {
+        	$valid['valid']=TRUE;
+			$data = array(
+				'email' => $this->input->post('email'),
+				'user' => $this->input->post('name'),
+				'Password'=> $this->input->post('password')
+			);
+		}
+	}
 }
