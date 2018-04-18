23f114a65792ec4803c23293d6772ae65acf20e4
Ahmed_Tolba <a.h.tolba95@gmail.com>
2018-04-18 07:18:04 -0700

    sign up basic front end

--- application/views/asusp.php
+++ application/views/asusp.php
@@ -34,6 +34,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		    </div>
 		  </div>
 		</nav>
+
+		<div class="container-fluid">
+		<form name="myForm" autocomplete="off" enctype="multipart/form-data" method="post" action="<?php echo base_url(); ?>Welcome/login">
+			<input type="text" name="username" class="form-control" placeholder="Username">
+			<input type="email" name="email" class="form-control" placeholder="E-Mail">
+			<input type="password" name="password" class="form-control" placeholder="Password">
+			<button type="submit">Submit</button>
+		</form>
+		</div>
 		
 </body>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/jquery-3.2.1.min.js"></script>
