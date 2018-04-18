189220e1d2ccea9b79e9db28369f50d3e2219ae9
Ahmed_Tolba <a.h.tolba95@gmail.com>
2018-04-18 07:05:39 -0700

    login front in

--- application/views/asusp.php
+++ application/views/asusp.php
@@ -36,6 +36,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		    </div>
 		  </div>
 		</nav>
+		<form name="myForm" autocomplete="off" enctype="multipart/form-data" method="post" action="<?php echo base_url(); ?>Welcome/login">
+			<input type="email" name="email" class="form-control" placeholder="E-Mail">
+			<input type="password" name="password" class="form-control" placeholder="Password">
+			<button type="submit">Submit</button>
+		</form>
 </body>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/jquery-3.2.1.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/bootstrap.min.js"></script>
