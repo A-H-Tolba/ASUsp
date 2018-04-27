6efb336005b5ab510ec69eefd346e7a8e9fcbff3
Nourhan <30366267+Nourhan-Nasr@users.noreply.github.com>
2018-04-27 23:10:28 +0200

    Adding_Searching_Form

--- application/views/home.php
+++ application/views/home.php
@@ -13,6 +13,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 	<div style="height: 45%"></div>
 	<h1 style="text-align: center;">Welcome <?php echo $session['email'] ?></h1>
+	<form action="<?php echo base_url(); ?>Welcome/search" method="post">
+	<input type="text" name="username" placeholder="search_by_userName">
+	<input type="submit" name="submit" value="Search">
+</form>
 </body>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/jquery-3.2.1.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/bootstrap.min.js"></script>
