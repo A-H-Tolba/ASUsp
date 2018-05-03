2fc1decdc99b75b91d7457f7059630de973f4608
Menna Allah Muhammed Abd el rahman <menna.muhammed198@gmail.com>
2018-05-03 11:26:53 +0200

    admin login

---
+++ application/views/admin_control.php
@@ -0,0 +1,4 @@
+<h1 style="text-align: center;">Admin Control Panel</h1>
+<form action="<?php echo base_url(); ?>Welcome/search" method="post">
+	<input type="text" name="username" placeholder="search_by_userName">
+	<input type="submit" name="submit" value="Search">
