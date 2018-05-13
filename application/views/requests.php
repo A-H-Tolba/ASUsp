0724babab46489aaf45729b50bcd9c9aff178d7f
Ahmed Adel <AhmedAdel94@users.noreply.github.com>
2018-05-13 23:32:39 +0200

    edits

--- application/views/requests.php
+++ application/views/requests.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <?php foreach(array_reverse($requests) as $request):?>
 
-    <a style="text-align: center;" href = "<?php echo $request['content'];?>">  </a>
+    <h2 class="text-center"><a class="text-center" style="text-align: center;" href = "<?php echo $request['content'];?>"> <?php echo $request['comments'];?> </a></h2>
 
 <?php endforeach; ?>
