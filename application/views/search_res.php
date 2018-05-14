a57a56de4bb480b35b9fa5d704dc6dfd6c29beed
Nourhan <30366267+Nourhan-Nasr@users.noreply.github.com>
2018-05-14 20:41:10 +0200

    editing_add_friend

--- application/views/search_res.php
+++ application/views/search_res.php
@@ -3,8 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 <?php foreach ($users as $post) { ?>
-			<a href="<?php echo base_url(); ?>Welcome/account?<?php echo $post->id ?>"><h1 style="text-align: center;"><?php echo $post->lname; ?>'s profile</h1></a>
-	<?php } ?>
+			<a href="<?php echo base_url(); ?>Welcome/account?<?php echo $post->id ?>"><h1 style="text-align: center;">
+				<?php echo $post->fname." ".$post->lname; ?>'s profile</h1></a>
+	<?php } ?><br>
+	<a href="<?php echo base_url(); ?>Welcome/pending_requests?c=<?php echo $post->id ?>"><h1 style="text-align: center;">
+				Add Friend</h1></a>
+	}?>
 </body>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/jquery-3.2.1.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/bootstrap.min.js"></script>
