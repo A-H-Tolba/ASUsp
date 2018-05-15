f8f5224d292b29ae95ec7935522f3c41f8cc29e2
Ahmed Adel <AhmedAdel94@users.noreply.github.com>
2018-05-15 11:08:13 +0200

    request list edits

--- application/views/requests.php
+++ application/views/requests.php
@@ -5,7 +5,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <h2 class="text-center">Requests</h2>
 <?php foreach(array_reverse($requests) as $request):?>
 
-    <h2 class="text-center"><a class="text-center" style="text-align: center;" href = "<?php echo $request['content'];?>"> <?php echo $request['comments'];?> </a></h2>
+    <h2 class="text-center"><a onclick="window.location='<?php echo site_url("Welcome/getFriend/".$request['likes']);?>'"><?php echo$request['comments'];?></a>
+</h2>
 
 <?php endforeach; ?>
 
