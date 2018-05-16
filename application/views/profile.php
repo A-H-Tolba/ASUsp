b2981137300f5e1249f7b4958d07b159a284f481
A-H-Tolba <a.h.tolba95@gmail.com>
2018-05-16 03:34:00 -0700

    Update profile.php

--- application/views/profile.php
+++ application/views/profile.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
@@ -17,7 +16,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <?php foreach(array_reverse($myposts) as $post):?>
 	    
 	<div id="rpost"><p><?php echo nl2br($post['post']);?></div>
-	<p style="text-align: center;"><?php echo nl2br($post['comments']);?></p>
+	<div id="rcomment"><p style="text-align: center;"><?php echo nl2br($post['comments']);?></p></div>
 	<!-- <button id = "commentBtn" onclick="return addComment()">Comment</button> -->
 		<div id="commentDiv" style="text-align: center;">
 		
@@ -37,43 +36,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
-=======
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-<!--
-	<h1 style="text-align: center;">Welcome <?php //echo $session['email'] ?></h1>
--->
-<form action="<?php echo base_url(); ?>Welcome/search" method="post">
-	<input type="text" name="username" placeholder="search_by_userName">
-	<input type="submit" name="submit" value="Search">
-</form>
-<hr>
-<img id="pp" src="<?php echo base_url()."/uploads/".$session['pic']; ?>"/>
-<p id="wp">Welcome to <?php echo $session['user_name']; ?>'s profile.</p>
-<br/>
-<p>Latest posts</p>
-<?php foreach(array_reverse($myposts) as $post):?>
-	    
-	<div id="rpost"><p><?php echo nl2br($post['post']);?></div>
-	<p style="text-align: center;"><?php echo nl2br($post['comments']);?></p>
-	<!-- <button id = "commentBtn" onclick="return addComment()">Comment</button> -->
-		<div id="commentDiv" style="text-align: center;">
-		
-		<?php echo form_open('Welcome/addComment/'.$post['id']); ?>
-		<input type="text" name = "comment" placeholder="add comment" >
-		<input type="submit" name="submit" value="Comment">
-		</form>
-	</div></p>
-		
-<br/> 
-        <?php endforeach; ?>
-</body>
-
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/bootstrap.min.js"></script>
-</html>
-
-
-
->>>>>>> 86a5de8776e4aa7177952fabd2023e70db5b0833
